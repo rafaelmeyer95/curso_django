ac63c86e4d454c61d76ae0497af7dce4af37cb1d
Rafael <rafael_meyer_@hotmail.com>
2021-09-11 23:29:27 -0300

    Criação do DetailView

--- blog/models.py
+++ blog/models.py
@@ -1,29 +1,41 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset()\
+                                           .filter(status='publicado')
 
 class Post(models.Model):
-    STATUS    = (
-                ('rascunho','Rascunho'),
-                ('publicado','Publicado'),
+    STATUS = (
+        ('rascunho','Rascunho'),
+        ('publicado','Publicado'),
     )
-    titulo    = models.CharField(max_length=250)
-    slug      = models.SlugField(max_length=250)  # https://sensorville.com.br/iiot-vs-covid/
-    autor     = models.ForeignKey(User,
-                              on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=250)
+    slug   = models.SlugField(max_length=250)#https://site.com/noticias/campeonato-brasileiro-2020/01/02/2020
+    autor  = models.ForeignKey(User,
+                               on_delete=models.CASCADE)
     conteudo  = models.TextField()
     publicado = models.DateTimeField(default=timezone.now)
     criado    = models.DateTimeField(auto_now_add=True)
     alterado  = models.DateTimeField(auto_now=True)
     status    = models.CharField(max_length=10,
-                              choices=STATUS,
-                              default='rascunho')
+                                 choices=STATUS,
+                                 default='rascunho')
+
+    objects   = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('post_detail',args=[self.slug])
 
     class Meta:
-        ordering = ('publicado',)
+        ordering = ('-publicado',)
 
     def __str__(self):
         return self.titulo
+
 
 # Create your models here.
